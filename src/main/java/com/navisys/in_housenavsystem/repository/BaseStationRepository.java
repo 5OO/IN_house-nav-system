24da3819f563bb59c885093eae28f4d386915809
5OO <git.hub@myrk.eu>
2023-05-15 14:08:47 +0300

    base station repository refactoring and added two methods

--- src/main/java/com/navisys/in_housenavsystem/repository/BaseStationRepository.java
+++ src/main/java/com/navisys/in_housenavsystem/repository/BaseStationRepository.java
@@ -1,5 +1,14 @@
 package com.navisys.in_housenavsystem.repository;
 
-public interface BaseStationRepository extends JPARepository {
+import com.navisys.in_housenavsystem.model.BaseStation;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
+public interface BaseStationRepository extends JpaRepository<BaseStation, UUID> {
+
+    Optional<BaseStation> findById(UUID id);
+
+    BaseStation save(BaseStation baseStation);
 }
