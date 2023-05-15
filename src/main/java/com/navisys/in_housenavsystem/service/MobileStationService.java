46c080b5dc3e3b303bfd8ca8023f41071ab38b11
5OO <git.hub@myrk.eu>
2023-05-15 14:17:55 +0300

    mobile  station repository refactoring and added one method

--- src/main/java/com/navisys/in_housenavsystem/service/MobileStationService.java
+++ src/main/java/com/navisys/in_housenavsystem/service/MobileStationService.java
@@ -1,5 +1,24 @@
 package com.navisys.in_housenavsystem.service;
 
-public class MobileStationService {
 
+import com.navisys.in_housenavsystem.model.MobileStation;
+import com.navisys.in_housenavsystem.repository.BaseStationRepository;
+import com.navisys.in_housenavsystem.repository.MobileStationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MobileStationService {
+    private final MobileStationRepository mobileStationRepository;
+    private final BaseStationRepository baseStationRepository;
+
+    @Autowired
+    public MobileStationService(MobileStationRepository mobileStationRepository, BaseStationRepository baseStationRepository) {
+        this.mobileStationRepository = mobileStationRepository;
+        this.baseStationRepository = baseStationRepository;
+    }
+
+    public MobileStation save(MobileStation mobileStation) {
+        return mobileStationRepository.save(mobileStation);
+    }
 }
