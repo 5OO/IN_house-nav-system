c20946b19941f29df67addd5bb110570628e70fc
5OO <git.hub@myrk.eu>
2023-05-15 12:34:24 +0300

    refactoring

--- src/main/java/com/navisys/in_housenavsystem/controller/BaseStationController.java
+++ src/main/java/com/navisys/in_housenavsystem/controller/BaseStationController.java
@@ -1,13 +1,22 @@
 package com.navisys.in_housenavsystem.controller;
 
-import org.springframework.http.HttpStatus;
+import com.navisys.in_housenavsystem.dto.BaseStationReportDto;
+import com.navisys.in_housenavsystem.service.BaseStationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@PostMapping("/basestation/report")
-@ResponseStatus(HttpStatus.CREATED)
+@RestController
 public class BaseStationController {
 
+    @Autowired
+    private BaseStationService service;
+
+    @PostMapping("/report")
+    public ResponseEntity<?> report(@RequestBody BaseStationReportDto report) {
+        service.processReport(report);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/navisys/in_housenavsystem/controller/MobileStationController.java
+++ src/main/java/com/navisys/in_housenavsystem/controller/MobileStationController.java
@@ -1,10 +1,29 @@
 package com.navisys.in_housenavsystem.controller;
 
+import com.navisys.in_housenavsystem.dto.MobileStationLocationDto;
+import com.navisys.in_housenavsystem.service.MobileStationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
 public class MobileStationController {
+    @Autowired
+    private MobileStationService service;
+
+    @GetMapping("/location/{uuid}")
+    public ResponseEntity<MobileStationLocationDto> getLocation(@PathVariable UUID uuid) {
+        MobileStationLocationDto location = service.getLocation(uuid);
+        if (location == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(location);
+    }
 
 }
