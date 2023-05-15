81538c40830a8873d3a92f3108c4028d8279abe0
5OO <git.hub@myrk.eu>
2023-05-15 12:29:06 +0300

    main method

--- src/main/java/com/navisys/in_housenavsystem/InHouseNavSystemApplication.java
+++ src/main/java/com/navisys/in_housenavsystem/InHouseNavSystemApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InHouseNavSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InHouseNavSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InHouseNavSystemApplication.class, args);
+    }
 
 }
