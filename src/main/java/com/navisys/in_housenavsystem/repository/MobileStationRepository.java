b8afa1813d6b2363e77f3da56ffd7a1c2df1bfd4
5OO <git.hub@myrk.eu>
2023-06-07 13:28:09 +0300

    implemented getLocation in mobile  station repository and service

--- src/main/java/com/navisys/in_housenavsystem/repository/MobileStationRepository.java
+++ src/main/java/com/navisys/in_housenavsystem/repository/MobileStationRepository.java
@@ -3,9 +3,13 @@ package com.navisys.in_housenavsystem.repository;
 import com.navisys.in_housenavsystem.model.MobileStation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MobileStationRepository extends JpaRepository<MobileStation, UUID> {
 
     MobileStation save(MobileStation mobileStation);
+
+    Optional<MobileStation> findById(UUID id);
+
 }
--- src/main/java/com/navisys/in_housenavsystem/service/MobileStationService.java
+++ src/main/java/com/navisys/in_housenavsystem/service/MobileStationService.java
@@ -8,6 +8,8 @@ import com.navisys.in_housenavsystem.repository.MobileStationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,6 +28,21 @@ public class MobileStationService {
     }
 
     public MobileStationLocationDto getLocation(UUID uuid) {
-        return null;
+        Optional<MobileStation> mobileStationOptional = mobileStationRepository.findById(uuid);
+
+        if (!mobileStationOptional.isPresent()) {
+            throw new NoSuchElementException("No MobileStation with id: " + uuid);
+        }
+
+        MobileStation mobileStation = mobileStationOptional.get();
+
+        MobileStationLocationDto dto = new MobileStationLocationDto();
+        dto.setMobileId(mobileStation.getId());
+        dto.setX(mobileStation.getLastKnownX());
+        dto.setY(mobileStation.getLastKnownY());
+
+        // TODO: Calculate error_radius, error_code, error_description
+
+        return dto;
     }
 }
